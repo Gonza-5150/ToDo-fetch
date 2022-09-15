10507ac1a2aa08ed83d1156bea79bc3b980b70f2
Gonza-5150 <estevesgonzalo@hotmail.com>
2022-09-15 01:31:12 +0000

    arreglos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,34 +10,46 @@ const Home = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    setToDo((prev) => [...prev, tarea]);
-	  setTarea("");
+    if (tarea != ""){
+      setToDo((prev) => [...prev, tarea]);
+      setTarea("");
+    }
+    
+    
   };
   console.log(toDo);
-  const elementDellete = (dIndex)=> {
-	setToDo(toDo.filter((e,i)=>i!=dIndex))
-  }
+  const elementDellete = (dIndex) => {
+    setToDo(toDo.filter((e, i) => i != dIndex));
+  };
 
   function limpiar() {
     document.getElementById("prueba").value = "";
-}
-
-  
+  }
 
   return (
     <div className="container">
       <div>
         <h1 className="text-center">ToDo's</h1>
         <form onSubmit={onSubmit}>
-          <div class="input-group d-flex justify-content-center">
-            <input type="text" id="prueba" placeholder="Tareas a realizar" onChange={(e) => setTarea(e.target.value)}/>
+          <div className="input-group d-flex justify-content-center">
+            <input
+              type="text"
+              id="prueba"
+              placeholder="Tareas a realizar"
+              value={tarea}
+              onChange={(e) => setTarea(e.target.value)}
+            />
           </div>
           <div>
             {toDo.map((element, dIndex) => {
               return (
                 <div className="d-flex justify-content-between my-3 col-4 mx-auto">
                   <p>{element}</p>
-                  <button type="button" class="btn btn-primary" onClick={()=>elementDellete(dIndex)}>
+                  <button
+                    type="button"
+                    className="btn btn-danger"
+                    onClick={() => elementDellete(dIndex)}
+                  >
                     borrar
                   </button>
                 </div>
